187108cf6c8ed0fb15f84f584f9090cf6f414218
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-03-07 03:32:31 -0800

    Docs, renaming, minor fixes.
    * Added documentation for both programs.
    * Renamed py-ipynb.sh to just py-ipynb to make it easier to run
    * Fixed minor inconsistencies in py-ipynb.
      - Fixed the filepath in the check if ipynb-generator existed
      - Updated the first error message to be consistent with the
        file name.

--- bin/ipynb-generator.c
+++ bin/ipynb-generator.c
@@ -8,6 +8,14 @@ int main(int argc, const char **argv) {
         exit(1);
     }
 
+    // LORE: The placeholder 'PYTHON-CODE-HERE' was previously named 'CODE HERE'. However, when the
+    // perl script would run, it would recursively replace the 'CODE HERE' with the content of the
+    // file since, in the python files, there is a 'YOUR CODE HERE'. Realizing this took about 45
+    // minutes. I will never get those 45 minutes back.
+    //
+    // File block template:
+    // The first block is the filename in Markdown. The placeholder is FILE-NAME-HERE....
+    // The second block is the python code. The placeholder is PYTHON-CODE-HERE.
     const char *file_block =
         "        {\n"
         "            \"cell_type\": \"markdown\",\n"
@@ -26,10 +34,14 @@ int main(int argc, const char **argv) {
         "            ]\n"
         "        }";
 
+    // The start of the ipynb template.
     const char* start =
         "{\n"
         "    \"cells\": [\n";
 
+    // The end of the ipynb template. It ends the "cells" array as well as appends the necessary
+    // metadata that ipynb needs. To be honest, I don't know what most of the metadata means so I
+    // just copy pasted it from a blank ipynb.
     const char *end =
         "    ],\n"
         "    \"metadata\": {\n"
@@ -55,6 +67,7 @@ int main(int argc, const char **argv) {
         "    \"nbformat_minor\": 5\n"
         "}";
 
+    // The target file pointer is passed in as the first argument (argv[1]).
     FILE *target_fp = fopen(argv[1], "w");
 
     if (!target_fp) {
@@ -62,13 +75,19 @@ int main(int argc, const char **argv) {
         exit(1);
     }
 
+    // Write the start of the template.
     fprintf(target_fp, "%s", start);
 
+    // Write n file blocks, where n is the number of python files to submit, passed in as the
+    // second argument (argv[2]).
     for (size_t i = 0; i < strtoul(argv[2], NULL, 10) - 1; i++) {
         fprintf(target_fp, "%s,\n", file_block);
     }
 
+    // Append the nth block but do NOT add a comma (,) at the end.
     fprintf(target_fp, "%s\n", file_block);
+
+    // Append the end of the template.
     fprintf(target_fp, "%s", end);
 
     return 0;
